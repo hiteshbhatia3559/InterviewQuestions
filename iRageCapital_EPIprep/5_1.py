7b2b08b50c3df6e6cb5740a6b3565adacd3cfa0c
hiteshbhatia3559 <31386280+hiteshbhatia3559@users.noreply.github.com>
2019-02-22 14:53:01 +0530

    Practice for iRageCapital interview

--- iRageCapital_EPIprep/5_1.py
+++ iRageCapital_EPIprep/5_1.py
@@ -4,8 +4,10 @@
 # that all elements less than A[r] (the "pivot") appear first, followed by elements equal to the pivot,
 # followed by elements greater than the pivot.
 
+# O(n) additional space complexity, O(n) time complexity
 
-def rearrange(a, i):
+
+def rearrange1(a, i):
     a_left, a_right, a_middle = [], [], []
     for item in a:
         if item < a[i]:
@@ -19,6 +21,30 @@ def rearrange(a, i):
 
 a = [1, 5, 6, 7, 3, 2, 4, 6, 9, 1, 3, 4, 5]
 i = 4
-print(rearrange(a, i))
+print(rearrange1(a, i))
 
-#output = [1, 2, 1, 3, 3, 5, 6, 7, 4, 6, 9, 4, 5]
+
+# output = [1, 2, 1, 3, 3, 5, 6, 7, 4, 6, 9, 4, 5]
+
+# O(1) additional space complexity, O(n) time complexity
+
+
+def rearrange2(a, i):
+    pivot = a[i]
+    smaller = 0
+    for item in range(len(a)):
+        if a[item] < pivot:
+            a[item], a[smaller] = a[smaller], a[item]
+            smaller += 1
+    larger = len(a) - 1
+    for item in reversed(range(len(a))):
+        if a[item] < pivot:
+            break
+        elif a[item] > pivot:
+            a[item], a[larger] = a[larger], a[item]
+            larger -= 1
+    return a
+
+
+print(rearrange2(a, i))
+
