a3247d1f4f1c493b4be70d732ac5a47cd30a42b4
hiteshbhatia3559 <31386280+hiteshbhatia3559@users.noreply.github.com>
2019-02-22 15:24:13 +0530

    Practice for iRageCapital interview

---
+++ iRageCapital_EPIprep/5_6.py
@@ -0,0 +1,13 @@
+# 5.6
+#
+# Write a program that takes an array denoting the daily stock price, and retums the maximum profit
+# that could be made by buying and then selling one share of that stock. There is no need to buy if
+# no profit is possible.
+
+
+def max_possible_profit(prices):
+    
+
+ticker = [310, 315, 275, 260, 270, 290, 230, 255, 250]
+
+print(max_possible_profit(ticker))
